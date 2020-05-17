acceb92be82ecbb6ed6ed95d0c2da263f2c60f9b
Maria Alvarez <mariajesus.microinformatica@gmail.com>
2020-05-17 10:46:27 +0200

    interfaz

---
+++ IFactura.java
@@ -0,0 +1,22 @@
+import Estados;
+
+public class IFactura {
+	
+	void cambiarEstado(Estados estado)
+	{
+		
+	}
+	
+	float getTotal()
+	{
+		return 0;
+		
+	}
+	
+	float getCantidadIva()
+	{
+		return 0;
+		
+	}
+}
+
